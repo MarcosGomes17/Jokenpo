0646b3212a8c625212ca3c93ff9e7d2b84ba690d
MarcosGomes17 <marcosvgomes07@outlook.com>
2025-05-30 19:01:10 -0300

    Update on reset button

--- scripts.js
+++ scripts.js
@@ -6,22 +6,25 @@ const resetButton = document.querySelector('#reset')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+resetButton.style.display = 'none'
+
 const playHuman = (humanChoice) => {
+
+    if (resetButton.style.display === 'none') {
+        resetButton.style.display = 'block'
+    }
 
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-
     const choices = ['rock', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3)
-
     return choices[randomNumber]
 }
 
 const playTheGame = (human, machine) => {
-
-    console.log('Humano: ' + human + " Maquina" + machine)
+    console.log('Humano: ' + human + " Maquina: " + machine)
 
     document.querySelectorAll('.choice').forEach(btn => {
         btn.classList.remove('machine-picked')
@@ -29,23 +32,17 @@ const playTheGame = (human, machine) => {
 
     document.querySelector(`#${machine}`).classList.add('machine-picked')
 
-
     if (human === machine) {
         result.innerHTML = "Deu empate!"
-    }
-
-    else if (
+    } else if (
         (human === 'rock' && machine === 'scissors') ||
         (human === 'paper' && machine === 'rock') ||
         (human === 'scissors' && machine === 'paper')
     ) {
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
-
         result.innerHTML = "Você ganhou!"
-    }
-
-    else {
+    } else {
         machineScoreNumber++
         machineScore.innerHTML = machineScoreNumber
         result.innerHTML = "Você perdeu para helena!"
@@ -53,7 +50,6 @@ const playTheGame = (human, machine) => {
 }
 
 resetButton.addEventListener('click', () => {
-
     humanScoreNumber = 0
     machineScoreNumber = 0
 
@@ -65,8 +61,10 @@ resetButton.addEventListener('click', () => {
         btn.classList.remove('machine-picked')
     })
 
-       setTimeout(() => {
+    resetButton.style.display = 'none'
+
+    setTimeout(() => {
         result.innerHTML = ''
     }, 3000)
-
 })
+
